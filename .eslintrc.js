1a6565123fafd08ffa316895c0ef7d900cd8b7e9
Sayeed Afridi <as.afridi563@gmail.com>
2021-08-22 03:55:54 +0600

    initial commit with necessary configs

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react-native/no-inline-styles': ['off'],
+  },
+};
